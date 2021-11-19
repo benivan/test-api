d3292aae0ae5dc5dcdc8773a0e576becbb53ee94
VIkash <itsmevikash09@gmail.com>
2021-11-19 20:36:59 +0530

    post dao

---
+++ src/main/java/com/example/springsecurity/Dao/Post.java
@@ -0,0 +1,39 @@
+package com.example.springsecurity.Dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String screamId;
+    private String body;
+    private String userHandle;
+    private String mediaUrl ;
+    private LocalDateTime createdAt;
+    private Integer likeCount;
+    private Integer unlikeCount;
+    private String imageUrl;
+    private Integer commentCount;
+    private Boolean isLiked;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Users user;
+    @OneToMany(fetch = FetchType.LAZY)
+    private Set<Like> likes = new HashSet<>();
+    @OneToMany(fetch = FetchType.LAZY)
+    private  List<Comment> comments = new ArrayList<>();
+
+}
