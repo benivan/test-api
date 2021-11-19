274f3afd56a8b7c537a7f32d56001b463dc81279
VIkash <itsmevikash09@gmail.com>
2021-11-19 20:37:18 +0530

    composite key

---
+++ src/main/java/com/example/springsecurity/Dao/LikedPKId.java
@@ -0,0 +1,19 @@
+package com.example.springsecurity.Dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Embeddable
+public class LikedPKId implements Serializable {
+    private Long userId;
+    private Long postId;
+}
