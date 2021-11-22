dda9c1ca4db659306f738f0bea090046cb03eb17
VIkash <itsmevikash09@gmail.com>
2021-11-22 23:45:16 +0530

    changes 0.1

--- src/main/java/com/example/springsecurity/SpringSecurityApplication.java
+++ src/main/java/com/example/springsecurity/SpringSecurityApplication.java
@@ -1,13 +1,55 @@
 package com.example.springsecurity;
 
+import com.example.springsecurity.Dao.Role;
+import com.example.springsecurity.Dao.Users;
+import com.example.springsecurity.Dto.UserDto;
+import com.example.springsecurity.Service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.beans.BeanProperty;
+import java.util.Collections;
+import java.util.Set;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner run(UserService userService) {
+        return args -> {
+            userService.saveRole(new Role(null,"ROLE_USER"));
+            userService.saveRole(new Role(null,"ROLE_MODERATOR"));
+            userService.saveRole(new Role(null, "ROLE_SENIOR_MODERATOR"));
+            userService.saveRole(new Role(null,"ROLE_ADMIN"));
+            userService.saveRole(new Role(null,"ROLE_SUPER_ADMIN"));
+
+            userService.saveUser(new Users(
+                    null,
+                    "userName",
+                    "FirstName",
+                    "LastName",
+                    "something@gmail.com",
+                    "123456",
+                    "contact",
+                    Collections.emptySet()
+            ));
+
+
+//            userService.saveUser(new Users(new UserDto("username1", "FirstName1", "LastName1", "something1@gmail.com", "123456", "contact1"
+//            )));
+//            userService.saveUser(new Users(new UserDto("username1", "FirstName1", "LastName1", "something1@gmail.com", "123456", "contact1"
+//            )));
+
+            userService.addRoleToUser("username","ROLE_USER");
+            userService.addRoleToUser("username","ROLE_MODERATOR");
+
+        };
     }
 
 }
