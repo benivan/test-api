df9954248bc4ab37b7fb925a2b04f3771f984d0b
VIkash <itsmevikash09@gmail.com>
2021-11-22 23:45:11 +0530

    changes 0.1

--- src/main/java/com/example/springsecurity/Dao/Role.java
+++ src/main/java/com/example/springsecurity/Dao/Role.java
@@ -4,10 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Data
@@ -18,4 +16,5 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
+
 }
